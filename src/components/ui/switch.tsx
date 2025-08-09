8e2dc4853c745c30fa3134474099fe9b95839b7e
iuval <iuvalgoldansky@gmail.com>
2025-08-08 22:13:15 -0300

    fix(ui/switch): correct peer classes so thumb returns on uncheck; ensure toggle can be turned off reliably

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { cn } from "@/lib/utils";
+import * as React from 'react';
+import { cn } from '@/lib/utils';
 
 interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
@@ -11,15 +11,13 @@ const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
   ({ className, ...props }, ref) => {
     return (
       <label className="relative inline-flex items-center cursor-pointer">
-        <input
-          type="checkbox"
-          className="sr-only peer"
-          ref={ref}
-          {...props}
-        />
+        <input type="checkbox" className="sr-only peer" ref={ref} {...props} />
         <div
           className={cn(
-            "w-11 h-6 bg-input peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-ring rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-background after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-primary",
+            // Track
+            'relative w-11 h-6 rounded-full bg-input transition-colors peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-ring peer-checked:bg-primary',
+            // Thumb
+            "after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:bg-background after:transition-transform peer-checked:after:translate-x-5",
             className
           )}
         />
@@ -28,6 +26,6 @@ const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
   }
 );
 
-Switch.displayName = "Switch";
+Switch.displayName = 'Switch';
 
-export { Switch }; 
+export { Switch };
